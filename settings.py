f3bc22b7e3b23d44e919af3c10500a897745e433
Raul Jimenez Ortega <raul@georemindme.com>
2011-06-14 02:38:39 +0200

    Setting up the templates, adding translations, etc.

--- settings.py
+++ settings.py
@@ -108,4 +108,20 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.request",
-    "django.core.context_processors.media")
+    "django.core.context_processors.media"
+)
+
+LANGUAGES = (
+  ('ca', u'Català'),
+  #('de', 'Deutsch'),
+  ('en', 'English'),
+  ('es', u'Español'),
+  #('fr', 'Francais'),
+  #('gl', 'Galego'),
+  #('it', 'Italiano'),
+  #('nl', 'Nederlands'),
+  #('pl', 'Polski'),
+  #('zh', 'Chinese'),
+  
+)
+
