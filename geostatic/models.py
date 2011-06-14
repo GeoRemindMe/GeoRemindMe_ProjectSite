70941694220b243745027da579c8b1907777e589
Ruben Dugo Martin <raubn.zero@gmail.com>
2011-06-14 10:21:48 +0200

    fixing error in model

--- geostatic/models.py
+++ geostatic/models.py
@@ -5,8 +5,8 @@ from django.contrib.sites.models import Site
 
 class Page(models.Model):
     slug = models.SlugField("Slug",blank=True,help_text=u"Blank for root url")
-    subdomain = models.CharField("Subdomain",max_length=255,)
-    template = models.CharField("Template",max_length=255,help_text=u"Blank for www (equal to no subdomain)")
+    subdomain = models.CharField("Subdomain",max_length=255,blank=True,help_text=u"Blank for www (equal to no subdomain)")
+    template = models.CharField("Template",max_length=255)
     
     def __unicode__(self):
         return unicode(self.slug)+u" on "+unicode(self.subdomain)
